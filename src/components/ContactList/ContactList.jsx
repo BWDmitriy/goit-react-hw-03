20e8747f0fc11ae5d23835e70b977ff4b63232fc
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-07 17:10:50 +0300

    icons

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,20 @@
+import { FaPhone } from "react-icons/fa6";
+import { IoPerson } from "react-icons/io5";
+
 export default function ContactList({ contacts, onDeleteContact }) {
  return (
     <ul className="contact-list">
       {contacts.map(contact => (
         <li key={contact.id}>
-          {contact.name}: {contact.number}
+          <div className="contact-list-div">
+            <span><IoPerson />{" "+contact.name}</span>
+            <span><FaPhone />{" "+contact.number}</span>
+          </div>
           <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
         </li>
       ))}
     </ul>
  );
 }
+
+
