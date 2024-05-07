9f6752da0dfb7eb67527f85d9bffe077d15c48ac
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-07 14:48:59 +0300

    Global update 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
 
 function App() {
  const [searchQuery, setSearchQuery] = useState('');
- const contactData = [
+ const [contacts, setContacts] = useState([
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
- ];
+ ]);
 
- const filteredContacts = contactData.filter(contact =>
+ const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(searchQuery.toLowerCase())
  );
+
+ const addContact = (name, number) => {
+    const contactObject = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts(prevContacts => [...prevContacts, contactObject]);
+ };
 
  return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm addContact={addContact} />
       <SearchBox searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <ContactList contacts={filteredContacts} />
     </div>
